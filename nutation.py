dfb79b0de5fbca072736ca97d98c500f7f5155b0
Cheva <ichevallier@unc.edu.ar>
2022-02-09 13:37:44 -0300

    Nutaciones

--- nutation.py
+++ nutation.py
@@ -1,19 +1,14 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python3.8
 '''
-    Description: corrects phase of FID and normalizes it considering the receiver
-    gain. It may also normalize by mass of 1H when given. Then plots FID and
-    transforms it to get spectrum in Hz and ppm. All the processed data will be
-    saved in ouput files (.csv). It may substract the background when given.
-
-    Notes: doesn't normalize the background by it mass yet (only by RG).
-
     Written by: Ignacio J. Chevallier-Boutell.
-    Dated: November, 2021.
+    Dated: February, 2022.
+    Log: doesn't normalize the background by it mass yet (only by RG).
 '''
 
 import argparse
 import numpy as np
 import pandas as pd
+from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 from cycler import cycler
 
@@ -41,13 +36,22 @@ plt.rcParams["figure.autolayout"] = True
 
 plt.rcParams["lines.linestyle"] = '-'
 
-def main():
+def damped_sine(t, offset, amplitude, decayRate, phShift, period):
+    return offset + amplitude * np.exp(- t / decayRate) * np.sin(np.pi * (t - phShift) / period)
 
-    Files = args.input
+def fit(t, magnetization, p0):
+    popt, pcov = curve_fit(damped_sine, t, magnetization, p0 = p0, bounds=(0, np.inf))
+    perr = np.sqrt(np.diag(pcov))
+    return popt, perr
+
+def main():
+    F_array = args.input
+    Out = args.output
+    p0 = args.InitVal
     t, M = [], []
     A = []
 
-    for F in Files:
+    for F in F_array:
         data = pd.read_csv(F, header = None, delim_whitespace = True).to_numpy()
         for k in range(np.shape(data)[0]):
             t.append(data[k, 0])
@@ -60,26 +64,47 @@ def main():
     t = t[S]
     M = M[S]
 
-    with open('nutation.csv', 'w') as f:
+    if p0 == None:
+        p0 = [10, 70, 20, 1, 10]
+        popt, perr = fit(t, M, p0)
+
+        fig, ax = plt.subplots()
+
+        ax.scatter(t, M, label='data', zorder=2)
+        ax.plot(t, damped_sine(t, *popt), label='fit', color='mediumseagreen', zorder=0)
+        ax.set_xlabel('t [us]')
+        ax.set_ylabel('M')
+        ax.legend()
+
+        plt.savefig(f'{Out}')
+
+    else:
+        popt, perr = fit(t, M, p0)
+
+        fig, ax = plt.subplots()
+
+        ax.scatter(t, M, label='data', zorder=2)
+        ax.plot(t, damped_sine(t, *popt), label='fit', color='mediumseagreen', zorder=0)
+        ax.set_xlabel('t [us]')
+        ax.set_ylabel('M')
+        ax.legend()
+
+        plt.savefig(f'{Out}')
+
+    with open(f'{Out}.csv', 'w') as f:
+        f.write("offset, amplitude, decayRate [us], phShift, period \n")
+        f.write(f'{popt[0]:.2f}, {popt[1]:.2f}, {popt[2]:.2f}, {popt[3]:.2f}, {popt[4]:.2f} \n\n')
+
         f.write("t [us], M \n")
         for i in range(len(t)):
             f.write(f'{t[i]:.4f}, {M[i]:.4f} \n')
 
-    fig, ax = plt.subplots()
-
-    ax.scatter(t, M)
-    ax.minorticks_on()
-    ax.grid(True, which='both')
-    plt.axhline(0, color='black', zorder=-2)
-    ax.set_xlabel('t [us]')
-    ax.set_ylabel('M')
-
-    plt.savefig('nutation')
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('input', help = "Path to the input file.", nargs = '+')
+    parser.add_argument('input', help = "Path to the nutation files.", nargs = '+')
+    parser.add_argument('output', help = "Path for the output file.")
+    parser.add_argument('-p0', '--InitVal', help = 'Initial guess for parameters: offset, amplitude, decayRate, phShift, period.', nargs = 5, type = float)
 
     args = parser.parse_args()
 
