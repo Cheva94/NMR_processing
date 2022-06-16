0836a2d9c8faed576b3be73cfa489f4ced9f35c2
Cheva <nacho.cheva@gmail.com>
2022-06-16 19:58:14 +0200

    .

--- core/SR_CPMG.py
+++ core/SR_CPMG.py
@@ -181,7 +181,7 @@ def plot(tau1, tau2, Z, T1, T2, S, M1, M2, Out, nLevel, T1min, T1max, T2min, T2m
     peaks2x, peaks2y = T2[peaks2], projT2[peaks2]
 
     topPeak2 = np.max(peaks2y)
-    ymax2 = topPeak2 + 0.2
+    ymax2 = topPeak2 + 0.3
 
     cumT2 = np.cumsum(projT2[15:-15])
     cumT2 /= cumT2[-1]
@@ -190,7 +190,7 @@ def plot(tau1, tau2, Z, T1, T2, S, M1, M2, Out, nLevel, T1min, T1max, T2min, T2m
     for i in range(len(peaks2x)):
         if peaks2y[i] > 0.1 * topPeak2:
             axs[0,2].plot(peaks2x[i], peaks2y[i] + 0.03, lw = 0, marker=11, color='black')
-            axs[0,2].annotate(f'{peaks2x[i]:.0f}', xy = (peaks2x[i], peaks2y[i] + 0.05), fontsize=30, ha = 'center')
+            axs[0,2].annotate(f'{peaks2x[i]:.0f}', xy = (peaks2x[i], peaks2y[i] + 0.1), fontsize=30, ha = 'center')
     axs[0,2].set_xlabel(r'$T_2$ [ms]')
     axs[0,2].set_xscale('log')
     axs[0,2].set_ylim(bottom=-0.05, top=ymax2)
@@ -216,36 +216,35 @@ def plot(tau1, tau2, Z, T1, T2, S, M1, M2, Out, nLevel, T1min, T1max, T2min, T2m
     peaks1x, peaks1y = T1[peaks1], projT1[peaks1]
 
     topPeak1 = np.max(peaks1y)
-    ymax1 = topPeak1 + 0.2
+    ymax1 = topPeak1 + 0.3
 
     cumT1 = np.cumsum(projT1[15:-15])
     cumT1 /= cumT1[-1]
 
-    axs[0,3].plot(T1[15:-15], projT1[15:-15], label = 'Distrib.', color = 'teal')
+    axs[1,3].plot(projT1[15:-15], T1[15:-15], label = 'Distrib.', color = 'teal')
     for i in range(len(peaks1x)):
         if peaks1y[i] > 0.1 * topPeak1:
-            axs[0,3].plot(peaks1x[i], peaks1y[i] + 0.03, lw = 0, marker=11, color='black')
-            axs[0,3].annotate(f'{peaks1x[i]:.0f}', xy = (peaks1x[i], peaks1y[i] + 0.05), fontsize=30, ha = 'center')
-    axs[0,3].set_xlabel(r'$T_1$ [ms]')
-    axs[0,3].set_xscale('log')
-    axs[0,3].set_ylim(bottom=-0.05, top=ymax1)
+            axs[1,3].plot(peaks1y[i] + 0.03, peaks1x[i], lw = 0, marker=4, color='black')
+            axs[1,3].annotate(f'{peaks1x[i]:.0f}', xy = (peaks1y[i] + 0.1, peaks1x[i]), fontsize=30, va = 'center')
+    axs[1,3].set_ylabel(r'$T_1$ [ms]')
+    axs[1,3].set_yscale('log')
+    axs[1,3].set_xlim(left=-0.05, right=ymax1)
 
-    ax = axs[0,3].twinx()
-    ax.plot(T1[15:-15], cumT1, label = 'Cumul.', color = 'coral')
-
+    ax = axs[1,3].twiny()
+    ax.plot(cumT1, T1[15:-15], label = 'Cumul.', color = 'coral')
     ref = cumT1[0]
     for x in range(len(cumT1)):
         if cumT1[x] < 0.01:
             continue
         elif (cumT1[x] - ref) < 0.0001:
-            ax.annotate(f'{100*cumT1[x]:.0f} %', xy = (T1[15:-15][-1], cumT1[x]), fontsize=30, ha='right', color='coral')
+            ax.annotate(f'{100*cumT1[x]:.0f} %', xy = (cumT1[x], T1[15:-15][-1]), fontsize=30, ha='right', color='coral')
         ref = cumT1[x]
-    ax.set_ylim(-0.1, 1.1)
-    ax.set_ylabel('Cumulative')
+    ax.set_xlim(-0.1, 1.1)
+    ax.set_xlabel('Cumulative')
 
-    lines1, labels1 = axs[0,3].get_legend_handles_labels()
+    lines1, labels1 = axs[1,3].get_legend_handles_labels()
     lines2, labels2 = ax.get_legend_handles_labels()
-    ax.legend(lines1 + lines2, labels1 + labels2)
+    ax.legend(lines1 + lines2, labels1 + labels2, loc='lower right')
 
     mini = np.max([T1min, T2min])
     maxi = np.min([T1max, T2max])
@@ -260,7 +259,7 @@ def plot(tau1, tau2, Z, T1, T2, S, M1, M2, Out, nLevel, T1min, T1max, T2min, T2m
     axs[1,2].set_yscale('log')
     axs[1,2].legend(loc='lower right')
 
-    axs[1,3].axis('off')
+    axs[0,3].axis('off')
 
     plt.savefig(f'{Out}')
 
