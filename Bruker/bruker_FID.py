d286eb124f1e2e05b4142ab5d1228efcf70d2550
Cheva <nacho.cheva@gmail.com>
2023-05-23 22:26:08 -0300

    Simplificación bruker FID

--- Bruker/bruker_FID.py
+++ Bruker/bruker_FID.py
@@ -7,7 +7,6 @@ import brukerPlot as graph
 def main():
 
     print('Analysing raw data...')
-
     fileDir = args.input
     Out = fileDir+'/'+'proc_FID'
 
@@ -16,21 +15,13 @@ def main():
     CS, spec = IO.spectrum(SGL, nP, SW)
 
     print('Writing processed data...')
-
     IO.writeFID(t, SGL, nP, CS, spec, Out)
 
     print('Plotting processed data...')
-
     graph.FID(t, SGL, nS, RDT, RG, att, RD, p90, CS, spec, Out)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
     parser.add_argument('input', help = "Path to the FID fileDir.")
-# #    parser.add_argument('output', help = "Path for the output fileDirs.")
-#     parser.add_argument('-back', '--background', help = "Path to de FID background fileDir.")
-#     parser.add_argument('-crop', '--croppedValues', help = "Number of values to avoid at the beginning the FID.", type = int, default=0)
-
     args = parser.parse_args()
-
     main()
