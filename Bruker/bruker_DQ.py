422ee5ceac5581e1fdec659c1dc2a4e8b83eeaed
Cheva <nacho.cheva@gmail.com>
2023-05-23 22:25:49 -0300

    Creación DQ bruker

---
+++ Bruker/bruker_DQ.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.10
+
+import argparse
+import brukerIO as IO
+import brukerPlot as graph
+
+def main():
+
+    # print('Analysing raw data...')
+    # fileDir = args.input
+    # Out = fileDir+'/'+'proc_FID'
+
+    # t, SGL, nP, SW, nS, RDT, RG, att, RD, p90 = IO.readFID(fileDir)
+    # SGL = IO.PhCorrFID(SGL)
+    # CS, spec = IO.spectrum(SGL, nP, SW)
+
+    # print('Writing processed data...')
+    # IO.writeFID(t, SGL, nP, CS, spec, Out)
+
+    # print('Plotting processed data...')
+    # graph.FID(t, SGL, nS, RDT, RG, att, RD, p90, CS, spec, Out)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help = "Path to the FID fileDir.")
+    args = parser.parse_args()
+    main()
