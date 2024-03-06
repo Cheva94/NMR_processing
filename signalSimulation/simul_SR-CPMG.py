705aa31ed0fdb60a9b8e5d099428571552a961d9
Cheva <ichevallier@unc.edu.ar>
2024-03-06 15:23:37 -0300

    simul update

--- signalSimulation/simul_SR-CPMG.py
+++ signalSimulation/simul_SR-CPMG.py
@@ -1,62 +1,180 @@
 #!/usr/bin/python3.10
 
-import argparse
-import core_IO as IO
-import core_Plot as graph
+import numpy as np
+np.random.seed(1994)
+import pandas as pd
+from scipy.signal import find_peaks
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 
+plt.rcParams["font.weight"] = "bold"
+plt.rcParams["font.size"] = 30
 
-def main():
+plt.rcParams["axes.labelweight"] = "bold"
+plt.rcParams["axes.linewidth"] = 2
 
-    path = args.path
-    root = path.split(".txt")[0]
-    alpha = args.alpha
-    T1min, T1max = args.T1Range[0], args.T1Range[1]
-    T2min, T2max = args.T2Range[0], args.T2Range[1]
-    
-    print('Reading SR-CPMG map raw data...')
-    tau1, tau2, SGL, nP1, nP2 = IO.read2Dsgl(path, root)
-    RDT, att, RG, nS, RD, p90, p180, tEcho, nEcho, nFID = IO.read2Dparams(root)
-    # tau2 = tau2[nFID+1:] # Discards FID part of signal + First CPMG point
-    tau2 = tau2[nFID:] # Discards FID part of signal + First CPMG point
-    S0, T1, T2, K1, K2 = IO.initKernel2D(nP1, nP2, tau1, tau2, 
-                                         T1min, T1max, T2min, T2max)
-    params = (rf'Acquisition: RDT = {RDT} $\mu$s | Atten = {att} dB | '
-              rf'RG = {RG} dB | nS = {nS} | RD = {RD} s | '
-              rf'p90 = {p90} $\mu$s | p180 = {p180} $\mu$s | '
-              rf'tE = {tEcho:.1f} ms | nE = {nEcho}')
+plt.rcParams['xtick.major.size'] = 10
+plt.rcParams['xtick.major.width'] = 2
+plt.rcParams['xtick.minor.size'] = 6
+plt.rcParams['xtick.minor.width'] = 1.5
 
-    print('Analysing CPMG raw data...')
-    Z = IO.PhCorr2D(SGL, nP1, nP2)
-    Z = IO.NormRG2D(Z, RG, nP1, nP2, nFID)
+plt.rcParams['ytick.major.size'] = 10
+plt.rcParams['ytick.major.width'] = 2
+plt.rcParams['ytick.minor.size'] = 6
+plt.rcParams['ytick.minor.width'] = 1.5
 
-    print(f'Starting NLI: Alpha = {alpha}.')
-    S, iter = IO.NLI_FISTA_2D(K1, K2, Z, alpha, S0)
-    if iter < 100000:
-        print('Inversion ready!')
-    else:
-        print('Warning!')
-        print('Maximum number of iterations reached!')
-        print('Try modifying T1Range/T2Range and/or alpha settings.')
+plt.rcParams["legend.frameon"] = False
+plt.rcParams["legend.fontsize"] = 30
 
-    print(f'Fitting NLI results in time domain...')
-    MLap_SR, MLap_CPMG = IO.fitLapMag_2D(tau1, tau2, T1, T2, S)
+plt.rcParams["figure.figsize"] = 12.5, 10
+plt.rcParams["figure.autolayout"] = True
 
-    print('Writing CPMG processed data...')
-    IO.writeSRCPMG(T1, T2, S, root)
+plt.rcParams["lines.linewidth"] = 4
 
-    print('Plotting CPMG processed data...')
-    graph.SRCPMG(tau1, tau2, Z, T1, T2, S, MLap_SR, MLap_CPMG, root, 
-                 alpha, T1min, T1max, T2min, T2max, params, tEcho)
+# Colores
+Verde = '#08a189' # = 08A189 = 8 161 137    (principal)
+Naranja = '#fa6210' # = FA6210 = 250 98 16    (secundario)
+Azul = '#3498db' # (terciario)
+Morado = '#6a1b9a' # (cuaternario)
+Gris = '#808080' # (alternativo)
+Negro = '#000000' # =  = 0 0 0 (base)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-path', type=str, default = 'SR-FID-CPMG.txt', 
-                        help = "Path to the CPMG signal.")
-    parser.add_argument('-alpha', type = float, default = 0.1, 
-                        help = "Tikhonov regularization parameter.")
-    parser.add_argument('-T1Range', nargs = 2, type = float, default = [-1, 4], 
-                        help = "Range to consider for T1 values.")
-    parser.add_argument('-T2Range', nargs = 2, type = float, default = [-1, 4], 
-                        help = "Range to consider for T2 values.")
-    args = parser.parse_args()
-    main()
+def NLI_FISTA_2D(K1, K2, Z, alpha, S):
+    '''
+    Numeric Laplace inversion, based on FISTA.
+    '''
+
+    K1TK1 = K1.T @ K1
+    K2TK2 = K2.T @ K2
+    K1TZK2 = K1.T @ Z @ K2
+    ZZT = np.trace(Z @ Z.T)
+
+    invL = 1 / (np.trace(K1TK1) * np.trace(K2TK2) + alpha)
+    factor = 1 - alpha * invL
+
+    Y = S
+    tstep = 1
+    lastRes = np.inf
+
+    for iter in range(100000):
+        term2 = K1TZK2 - K1TK1 @ Y @ K2TK2
+        Snew = factor * Y + invL * term2
+        Snew[Snew<0] = 0
+
+        tnew = 0.5 * (1 + np.sqrt(1 + 4 * tstep**2))
+        tRatio = (tstep - 1) / tnew
+        Y = Snew + tRatio * (Snew - S)
+        tstep = tnew
+        S = Snew
+
+        if iter % 1000 == 0:
+            TikhTerm = alpha * np.linalg.norm(S)**2
+            ObjFunc = ZZT - 2 * np.trace(S.T @ K1TZK2) + np.trace(S.T @ K1TK1 @ S @ K2TK2) + TikhTerm
+
+            Res = np.abs(ObjFunc - lastRes) / ObjFunc
+            lastRes = ObjFunc
+            print(f'\t# It = {iter} >>> Residue = {Res:.6f}')
+
+            if Res < 1E-5:
+                break
+
+    return S, iter
+
+################################################################################
+
+alpha = 0.1
+T1s = np.array([10, 100]) # ms
+T2s = np.array([10, 100]) # ms
+Amps = np.array([1, 1]) # ([0.3, 0.7])
+T1min, T1max = 0.5, 2.5
+T2min, T2max = 0.5, 2.5
+
+largo1 = 20
+largo2 = 100
+tau1 = np.logspace(0, 3, largo1) # ms
+nP1 = len(tau1)
+tau2 = np.logspace(0, 3, largo2) # ms
+nP2 = len(tau2)
+
+SGL0 = np.zeros((nP1, nP2))
+SGL1 = np.zeros((nP1, nP2))
+for i in range(nP1):
+    for j in range(nP2):
+        SGL0[i, j] = Amps[0] * (1 - np.exp(-tau1[i]/T1s[0])) * np.exp(-tau2[j]/T2s[0])
+        SGL1[i, j] = Amps[1] * (1 - np.exp(-tau1[i]/T1s[1])) * np.exp(-tau2[j]/T2s[1])
+SGLnoisynt = SGL0 + SGL1
+
+noise = np.random.normal(0, 0.015, (largo1, largo2))
+SGLnoisy = SGLnoisynt + noise
+
+nBinx = nBiny = 300
+S0 = np.ones((nBinx, nBiny))
+T1 = np.logspace(T1min, T1max, nBinx)
+T2 = np.logspace(T2min, T2max, nBiny)
+
+K1 = np.zeros((nP1, nBinx))
+for i in range(nP1):
+    K1[i, :] = 1 - np.exp(-tau1[i] / T1)
+K2 = np.zeros((nP2, nBiny))
+for i in range(nP2):
+    K2[i, :] = np.exp(-tau2[i] / T2)
+
+Snoisynt, _ = NLI_FISTA_2D(K1, K2, SGLnoisynt, alpha, S0)
+np.savetxt('simul_SR-CPMG_sinRuido_Laplace.txt', Snoisynt)
+Snoisy, _ = NLI_FISTA_2D(K1, K2, SGLnoisy, alpha, S0)
+np.savetxt('simul_SR-CPMG_conRuido_Laplace.txt', Snoisy)
+# Snoisy = pd.read_csv('simul_SR-CPMG_conRuido_Laplace.txt', header = None, delim_whitespace=True).to_numpy()
+# Snoisynt = pd.read_csv('simul_SR-CPMG_sinRuido_Laplace.txt', header = None, delim_whitespace=True).to_numpy()
+
+###################################################################### Sin ruido
+print('Gráficos sin ruido')
+_, axs = plt.subplots(subplot_kw={"projection": "3d"})
+tau2, tau1 = np.meshgrid(tau2, tau1)
+axs.plot_surface(tau1, tau2, SGLnoisynt, cmap='viridis', rcount=100, ccount=100)
+
+axs.set_xlabel(r'$\tau_1$ [s]', fontsize=15)
+axs.set_ylabel(r'$\tau_2$ [s]', fontsize=15)
+axs.set_zlabel('Señal SR-CPMG', fontsize=15)
+axs.view_init(elev=10, azim=140)
+
+plt.savefig('simul_SR-CPMG_sinRuido_TimeDomain')
+
+_, axs = plt.subplots()
+Snorm = Snoisynt / np.max(Snoisynt)
+# Snorm = np.where(Snorm<0.15, 0.0, Snorm)
+
+axs.contour(T2, T1, Snorm, 100, cmap='rainbow')
+axs.set_xlabel(r'$T_2$ [ms]')
+axs.set_ylabel(r'$T_1$ [ms]')
+axs.set_xlim(10.0**T2min, 10.0**T2max)
+axs.set_ylim(10.0**T1min, 10.0**T1max)
+axs.set_xscale('log')
+axs.set_yscale('log')
+
+plt.savefig('simul_SR-CPMG_sinRuido_Laplace')
+
+###################################################################### Con ruido
+print('Gráficos con ruido')
+_, axs = plt.subplots(subplot_kw={"projection": "3d"})
+axs.plot_surface(tau1, tau2, SGLnoisy, cmap='viridis', rcount=100, ccount=100)
+
+axs.set_xlabel(r'$\tau_1$ [s]', fontsize=15)
+axs.set_ylabel(r'$\tau_2$ [s]', fontsize=15)
+axs.set_zlabel('Señal SR-CPMG', fontsize=15)
+axs.view_init(elev=10, azim=140)
+
+plt.savefig('simul_SR-CPMG_conRuido_TimeDomain')
+
+_, axs = plt.subplots()
+Snorm = Snoisy / np.max(Snoisy)
+# Snorm = np.where(Snorm<0.15, 0.0, Snorm)
+
+axs.contour(T2, T1, Snorm, 100, cmap='rainbow')
+axs.set_xlabel(r'$T_2$ [ms]')
+axs.set_ylabel(r'$T_1$ [ms]')
+axs.set_xlim(10.0**T2min, 10.0**T2max)
+axs.set_ylim(10.0**T1min, 10.0**T1max)
+axs.set_xscale('log')
+axs.set_yscale('log')
+
+plt.savefig('simul_SR-CPMG_conRuido_Laplace')
